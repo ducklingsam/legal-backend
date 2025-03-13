c7519aa4e5579c9a65dab02b8f71b340ef7972c0
ducklingsam <34626043+ducklingsam@users.noreply.github.com>
2025-03-13 20:09:14 +0300

    endpoints

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 
-@app.post('/search')
+@app.post('/text')
 async def search(request: Request):
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content={"detail": "No content"})
 
 
-@app.post('/scan')
+@app.post('/document')
 async def scan(request: Request):
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content={"detail": "No content"})
